82484983736004e44fd33b3f03121126b093602c
Tony Faller <49420290+amfaller@users.noreply.github.com>
2021-04-15 15:59:33 -0400

    Resized mainwindow for BBB screen

--- build-Mochi-Pounders-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-Mochi-Pounders-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -36,28 +36,28 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(480, 272);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         HammerButton_Red = new QPushButton(centralwidget);
         HammerButton_Red->setObjectName(QString::fromUtf8("HammerButton_Red"));
-        HammerButton_Red->setGeometry(QRect(0, 80, 71, 481));
+        HammerButton_Red->setGeometry(QRect(0, 100, 71, 111));
         HammerButton_Blue = new QPushButton(centralwidget);
         HammerButton_Blue->setObjectName(QString::fromUtf8("HammerButton_Blue"));
-        HammerButton_Blue->setGeometry(QRect(730, 80, 75, 481));
+        HammerButton_Blue->setGeometry(QRect(410, 100, 71, 111));
         ScoreCounter_Red = new QLCDNumber(centralwidget);
         ScoreCounter_Red->setObjectName(QString::fromUtf8("ScoreCounter_Red"));
         ScoreCounter_Red->setGeometry(QRect(0, 0, 71, 81));
         ScoreCounter_Blue = new QLCDNumber(centralwidget);
         ScoreCounter_Blue->setObjectName(QString::fromUtf8("ScoreCounter_Blue"));
-        ScoreCounter_Blue->setGeometry(QRect(730, 0, 71, 81));
+        ScoreCounter_Blue->setGeometry(QRect(410, 0, 71, 81));
         TimeCounter = new QLCDNumber(centralwidget);
         TimeCounter->setObjectName(QString::fromUtf8("TimeCounter"));
-        TimeCounter->setGeometry(QRect(370, 0, 71, 81));
+        TimeCounter->setGeometry(QRect(210, 0, 71, 81));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 21));
+        menubar->setGeometry(QRect(0, 0, 480, 21));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
